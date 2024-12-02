b804921e7651e928bae9641b64672ebf33d92625
Matheus Bezerra Lima <matheusbeducation@gmail.com>
2024-12-02 00:55:36 -0300

    up

--- js/teste.js
+++
@@ -1,9 +0,0 @@
-const teste = async() =>{
-    const data = await fetch('https://pokeapi.co/api/v2/pokemon/shuckle/')
-    const response = await data.json()
-    console.log(response);
-    
-    console.log(response);
-    
-}
-teste()
